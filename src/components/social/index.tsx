5e921e4ca1694702b15e50da3972032966caa2f6
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-22 17:27:26 +0700

    social link component

---
+++ src/components/social/index.tsx
@@ -0,0 +1,24 @@
+import { Link, Tooltip } from '@nextui-org/react';
+import { IconType } from 'react-icons';
+
+type Props = {
+    content: string;
+    linkColor?: string;
+    Icon: IconType;
+    iconColor: string;
+    link: string;
+}
+
+export const Social = ({content, linkColor, Icon, iconColor, link}: Props) => {
+    return (
+        <Tooltip showArrow={true} content={content}>
+            <Link 
+                href={link}
+                target='_blank'
+                className={`r-circle ${linkColor} w-[46px] h-[46px] flex items-center justify-center`}
+            >
+                <Icon color={iconColor} className="w-[120%] h-[120%]" />
+            </Link>
+        </Tooltip>
+    )
+}
