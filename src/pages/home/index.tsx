9366ba536187473fd3a7d5fc78338b87772c8ee6
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-22 17:26:09 +0700

    header integration in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Banner } from '../../components/Banner'
+import { Header } from '../../components/header'
 
 export const Home = () => {
+  const menuArr = [
+    {
+      id: '12',
+      path: '#service',
+      name: 'услуги'
+    },
+    {
+      id: '123',
+      path: '#form',
+      name: 'оставить заявку'
+    }
+  ]
   return (
-    <div>Home</div>
+    <>
+      <Header list={menuArr} />
+    </>
   )
 }
